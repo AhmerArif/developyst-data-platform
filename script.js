00c987b7c11cae27a2e3484f2416c8d3173212f5
Ahmer Arif <ahmer.arif@gmail.com>
2012-09-27 17:05:10 +0500

    Iteration 3
    Added highcharts
    Added Spatial Histograms
    Added Heatmaps

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var TileJSONs = [
     'http://a.tiles.mapbox.com/v3/developyst.pmiu_teacher_attendance.jsonp',
 	'http://a.tiles.mapbox.com/v3/developyst.punjab_teacher_presence.jsonp',
     'http://a.tiles.mapbox.com/v3/developyst.pmiu_student_attendance.jsonp',
+    'http://a.tiles.mapbox.com/v3/developyst.school_density.jsonp',
+	'http://a.tiles.mapbox.com/v3/developyst.total_school_heatmap.jsonp',
+	'http://a.tiles.mapbox.com/v3/developyst.total_school_density_sh_small.jsonp'
 ];
 
 $('#map').mapbox(TileJSONs, function(map, tiledata) {
@@ -12,6 +15,9 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
     map.getLayerAt(1).named('teacher_attendance_change');
     map.getLayerAt(2).named('teacher_attendance');
     map.getLayerAt(3).named('student_attendance');
+    map.getLayerAt(4).named('total_school_concentration_spatial_histogram_large');
+    map.getLayerAt(5).named('total_school_concentration_heatmap');
+    map.getLayerAt(6).named('total_school_concentration_spatial_histogram_small');
 
     // Don't composite base layer with other layers
     map.getLayer('base').composite(false);
@@ -20,6 +26,9 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
     map.disableLayer('teacher_attendance');
     map.disableLayer('teacher_attendance_change');
     map.disableLayer('student_attendance');
+    map.disableLayer('total_school_concentration_spatial_histogram_large');
+	map.disableLayer('total_school_concentration_heatmap');
+    map.disableLayer('total_school_concentration_spatial_histogram_small');
 	map.interaction.auto().off('on').off('off').on(wax.movetip().parent(map.parent).events());
 	map.ui.refresh();
 
